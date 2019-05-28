a953816af337ddc05b303802ff5e5ad6a9b8dd59
Njeri <njericooper@gmail.com>
2019-05-28 12:57:11 -0500

    Made Mobile Menu

--- assets/hamMenu/hamMenu.js
+++ assets/hamMenu/hamMenu.js
@@ -1,3 +1,22 @@
-function myFunction(x) {
-    x.classList.toggle("change");
-  }
+(function($) {
+  "use strict";
+
+  var $navbar = $(".nav"),
+      y_pos = $navbar.offset().top,
+      height = $navbar.height();
+
+  //scroll top 0 sticky
+  $(document).scroll(function() {
+      var scrollTop = $(this).scrollTop();
+      if (scrollTop > 0) {
+        $navbar.addClass("sticky");
+      } else {
+        $navbar.removeClass("sticky");  
+      }
+  });
+
+})(jQuery, undefined);
+
+$(".menu").click(function(){
+$("#nav").toggleClass("open");
+});
